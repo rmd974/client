e433135c71aea66012a84637537b1fb495cbec37
Favre Anael <anael.favre@gmail.com>
2016-04-18 21:40:00 +0200

    added test con with server

--- src/index.js
+++ src/index.js
@@ -11,3 +11,10 @@ class Game extends Phaser.Game {
 }
 
 new Game();
+
+var server = window.location.hostname + ':' + (window.location.port - 1)
+var server = 'ws://192.168.99.100:32798/'
+
+var conn = new WebSocket(server)
+conn.onopen = () => console.log("Connection established!")
+conn.onmessage = (e) => console.log(e.data)
